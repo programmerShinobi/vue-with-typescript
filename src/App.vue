ca451b5cbcfcaff41b7ef5f9a19de5534ebf0300
programmerShinobi <faqihpratamamuhti@gmail.com>
2022-10-31 05:24:37 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <header>
+      <div class="title">
+        <img src="./assets/heart.svg" alt="site logo">
+        <h1>Hyrule Jobs</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">Order by title</button>
+        <button @click="handleClick('salary')">Order by salary</button>
+        <button @click="handleClick('location')">Order by location</button>
+      </div>
+    </header>
+
+    <JobList :jobs="jobs" :order="order" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, ref } from 'vue';
+import Job from '@/types/Job'
+import OrderTerm from '@/types/OrderTerm'
+import JobList from './components/JobList.vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components: { JobList },
+  setup() {
+    const jobs = ref<Job[]>([
+      { title: 'web developer', location: 'jakarta ', salary: 30000, id: '1' },
+      { title: 'data analyst', location: 'bandung', salary: 40000, id: '2' },
+      { title: 'cyber security', location: 'yogyakarta', salary: 35000, id: '3' },
+      { title: 'ui/ux designer', location: 'semarang', salary: 21000, id: '4' },
+      { title: 'java developer', location: 'surabaya', salary: 32000, id: '5' }
+    ])
+    const order = ref<OrderTerm>('title')
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+
+    return { jobs, order, handleClick }
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
+  header .title{
+    display: flex;
+    justify-content: center;
+  }
+  header img {
+    width: 60px;
+    margin-right: 20px;
+  }
+  header h1 {
+    font-size: 3em;
+  }
 </style>
